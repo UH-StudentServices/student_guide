333f9bdf8ac9b84cd1786f930db251b0d75e79b9
lviit <lauri.viitala@wunderkraut.com>
2016-12-21 11:44:50 +0200

    HUB-32: tweaks, changelog

--- modules/uhsg_some_links/src/Plugin/Block/SomeLinksBlock.php
+++ modules/uhsg_some_links/src/Plugin/Block/SomeLinksBlock.php
@@ -18,19 +18,8 @@ class SomeLinksBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-  	$entities = entity_load_multiple('some_links');
-  	$some_links = array();
-    /*
-  	foreach ($entities as $entity) {
-  		$some_links[] = array(
-  			'icon' => $entity->icon_class->getValue(),
-  			'url' => $entity->url->getValue()
-  		);
-  	} */
     return array(
-        '#theme' => 'some_links',
-        //'some_links' => $some_links,
-        'entities' => $entities
+      'entities' => entity_load_multiple('some_links')
     );
  	}
 }
