e7558321c7e8a4934fd576f6f4a0b1428f2317bc
lviit <lauri.viitala@wunderkraut.com>
2016-12-27 15:42:51 +0200

    HUB-51: make autocomplete work

--- themes/uhsg_theme/js/menu_toggle.js
+++ themes/uhsg_theme/js/menu_toggle.js
@@ -3,12 +3,13 @@
 
   Drupal.behaviors.menu_toggle = {
     attach: function(context, settings) {
+      $('.block-views-blockdegree-programmes-block-1').once().find('.degree-programme-switcher__container').addClass('visually-hidden');
       $('.degree-programme-switcher__toggle', '.degree-programme-switcher').once().on('click', function() {
-      	var toggleText = $(this).hasClass('collapsed') ? Drupal.t('Change') : Drupal.t('Close');
-      	$(this).toggleClass('collapsed')
-      		.text(toggleText)
-      		.next('.degree-programme-switcher__container')
-      		.toggleClass('visually-hidden');
+        var toggleText = $(this).hasClass('collapsed') ? Drupal.t('Change') : Drupal.t('Close');
+        $(this).toggleClass('collapsed')
+          .text(toggleText)
+          .next('.degree-programme-switcher__container')
+          .toggleClass('visually-hidden');
       });
     }
   };
