24d0f835306cd2264b89bbb877d8dab99d2a945b
lviit <lauri.viitala@wunderkraut.com>
2016-12-30 18:10:01 +0200

    HUB-59: cookie consent module with theming

---
+++ modules/uhsg_cookie_consent/js/uhsg_cookie_consent.js
@@ -0,0 +1,10 @@
+(function ($) {
+  'use strict';
+
+  Drupal.behaviors.uhsg_cookie_consent = {
+    attach: function(context, settings) {
+    	window.cookieconsent_options = settings.uhsg_cookie_consent.options;
+    }
+  };
+
+}(jQuery));
