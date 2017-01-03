5d307564909bb1cad0ff1d81661c85a8aac9c6dd
lviit <lauri.viitala@wunderkraut.com>
2017-01-03 23:13:54 +0200

    HUB-38: theming

--- themes/uhsg_theme/js/feedback_form.js
+++ themes/uhsg_theme/js/feedback_form.js
@@ -4,7 +4,7 @@
   Drupal.behaviors.feedbackForm = {
     attach: function(context, settings) {
       $('.feedback-form__toggle', '.feedback-form').once().on('click', function() {
-        $(this).toggleClass('active').next('form').toggleClass('visually-hidden');
+        $(this).toggleClass('active').next('#feedback-form__content').children('form').toggleClass('visually-hidden');
         $(this).hasClass('active') ?
           $(this).children('.feedback-form__icon').removeClass('icon--chat icon-2x').addClass('icon--remove') :
           $(this).children('.feedback-form__icon').removeClass('icon--remove').addClass('icon--chat icon-2x');
