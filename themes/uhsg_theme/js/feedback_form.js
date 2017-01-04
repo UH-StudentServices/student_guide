6147218670260c537e58428558c006629860160a
lviit <lauri.viitala@wunderkraut.com>
2017-01-04 12:59:58 +0200

    HUB-38: theming, tweaks

--- themes/uhsg_theme/js/feedback_form.js
+++ themes/uhsg_theme/js/feedback_form.js
@@ -9,6 +9,11 @@
           $(this).children('.feedback-form__icon').removeClass('icon--chat icon-2x').addClass('icon--remove') :
           $(this).children('.feedback-form__icon').removeClass('icon--remove').addClass('icon--chat icon-2x');
       });
+      $('#edit-field-feedback-respond-value', '.feedback-form').prop('checked') && $('.form-item-mail', '.feedback-form').once().addClass('visually-hidden');
+      $('#edit-field-feedback-respond-value', '.feedback-form').once().change(function() {
+        $('.form-item-mail', '.feedback-form').toggleClass('visually-hidden');
+      });
+
     }
   };
 
