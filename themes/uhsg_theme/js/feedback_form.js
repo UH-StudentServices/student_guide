35637645adc0b2cc172718f480539941eb2ffa92
lviit <lauri.viitala@wunderkraut.com>
2017-01-04 14:04:37 +0200

    HUB-38: tweaks

--- themes/uhsg_theme/js/feedback_form.js
+++ themes/uhsg_theme/js/feedback_form.js
@@ -9,11 +9,11 @@
           $(this).children('.feedback-form__icon').removeClass('icon--chat icon-2x').addClass('icon--remove') :
           $(this).children('.feedback-form__icon').removeClass('icon--remove').addClass('icon--chat icon-2x');
       });
-      $('#edit-field-feedback-respond-value', '.feedback-form').prop('checked') && $('.form-item-mail', '.feedback-form').once().addClass('visually-hidden');
+      $('#edit-field-feedback-respond-value', '.feedback-form').prop('checked') == false && $('.form-item-mail', '.feedback-form').once().addClass('visually-hidden');
       $('#edit-field-feedback-respond-value', '.feedback-form').once().change(function() {
+        $(this).prop('checked') == false && $('#edit-mail', '.feedback-form').val('');
         $('.form-item-mail', '.feedback-form').toggleClass('visually-hidden');
       });
-
     }
   };
 
