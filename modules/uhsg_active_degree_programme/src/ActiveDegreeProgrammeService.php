d6bec71377840094afac9add3f5c36aed4d37f8d
lviit <lauri.viitala@wunderkraut.com>
2017-01-18 17:27:51 +0200

    clean service

--- modules/uhsg_active_degree_programme/src/ActiveDegreeProgrammeService.php
+++ modules/uhsg_active_degree_programme/src/ActiveDegreeProgrammeService.php
@@ -12,16 +12,35 @@ class ActiveDegreeProgrammeService {
   /**
    * Return name of active degree programme.
    */
-  public function getActiveDegreeProgramme() {
+  public function getName() {
     $node = \Drupal::routeMatch()->getParameter('node');
-    $term_from_route = \Drupal::routeMatch()->getParameter('taxonomy_term');
+    $term = \Drupal::routeMatch()->getParameter('taxonomy_term');
 
-    // if page is node first look if degree programme cookie matches any of the referenced degree programmes and use it.
-    // if no match is found, set the first degree programme reference as the new active one.
+    // if page is node and has a valid term, use it
+    if ($node && $this->getFromNode($node)) {
+      return $this->getFromNode($node);
+    }
+
+    // if page is degree programme term, get name from there
+    if ($term && $term->bundle() == 'degree_programme') {
+      return $this->getFromTerm($term);
+    }
+
+    // if page doesn't have a degree programme reference, or it isn't a degree programme term,
+    // read active degree programme from cookie. If no cookie fallback to "select degree programme".
+    if (isset($_COOKIE['Drupal_visitor_degree_programme'])) {
+      return $this->getFromCookie($_COOKIE['Drupal_visitor_degree_programme']);
+    }
+
+    return NULL;
+  }
+
+  private function getFromNode($node) {
     $reference_fields = array('field_article_degree_programme', 'field_news_degree_programme');
     foreach ($reference_fields as $reference_field) {
       if ($node && $node->hasField($reference_field) && isset($node->get($reference_field)->target_id)) {
 
+        // if cookie is set, search for match in reference field
         if (isset($_COOKIE['Drupal_visitor_degree_programme'])) {
           foreach ($node->$reference_field as $item) {
             if ($item->target_id == $_COOKIE['Drupal_visitor_degree_programme']) {
@@ -31,6 +50,7 @@ class ActiveDegreeProgrammeService {
           }
         }
 
+        //if no match is found, set the first degree programme reference as the new active one.
         if (empty($term)) {
           $term = \Drupal\taxonomy\Entity\Term::load($node->get($reference_field)->target_id);
         }
@@ -43,29 +63,22 @@ class ActiveDegreeProgrammeService {
         return $term_translated->getName();
       }
     }
-
-    // if page is degree programme term, make that degree programme active
-    if ($term_from_route && $term_from_route->bundle() == 'degree_programme') {
-      $term = $term_from_route;
-      $tid = $term->id();
-      $cookie = array('degree_programme' => $tid);
-      user_cookie_save($cookie);
-      return $term->getName();
-    }
-
-    // if page doesn't have a degree programme reference, or it isn't a degree programme term,
-    // read active degree programme from cookie. If no cookie fallback to "select degree programme".
-    else {
-      if (isset($_COOKIE['Drupal_visitor_degree_programme'])) {
-        $term = \Drupal\taxonomy\Entity\Term::load($_COOKIE['Drupal_visitor_degree_programme']);
-        if ($term) {
-          $term_translated = \Drupal::service('entity.repository')->getTranslationFromContext($term);
-          return $term_translated->getName();
-        }
-      }
-    }
-
     return NULL;
+  }
+
+  private function getFromTerm($term) {
+    $tid = $term->id();
+    $cookie = array('degree_programme' => $tid);
+    user_cookie_save($cookie);
+    return $term->getName();
+  }
+
+  private function getFromCookie($cookie) {
+    $term = \Drupal\taxonomy\Entity\Term::load($cookie);
+    if ($term) {
+      $term_translated = \Drupal::service('entity.repository')->getTranslationFromContext($term);
+      return $term_translated->getName();
+    }
   }
  
 }
