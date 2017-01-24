c8992709e2acb06951b180d03a91c70b20d2bd88
lviit <lauri.viitala@wunderkraut.com>
2017-01-24 16:58:31 +0200

    HUB-37: ugly prelim semi working my searches list.

--- modules/uhsg_search/js/uhsg-search.js
+++ modules/uhsg_search/js/uhsg-search.js
@@ -10,7 +10,38 @@
     	$('#views-exposed-form-search-block-1 input.button--reset').click(function (e) {
     	  e.preventDefault();
     	  location.reload();
-      })
+      });
+
+      //retrieve searches from cookie and display them
+      if ($.cookie('my_searches')) {
+        var my_searches = JSON.parse($.cookie('my_searches'));
+        my_searches.reverse();
+        var content = my_searches.map(function(value) {
+          return '<li class="list-of-links__link button--action-before icon--search theme-transparent">' + value + '</li>';
+        });
+        var title = '<h3>' + Drupal.t('My Searches') + '</h3>';
+        $('.my-searches').empty();
+        $('.my-searches').append(title + '<ul class="list-of-links">' + content + '</ul>');
+      }
+
+      // store searches in cookie
+      $('#edit-submit-search', '#views-exposed-form-search-block-1').on('click', function() {
+        if ($.cookie('my_searches')) {
+          var my_searches = JSON.parse($.cookie('my_searches'));
+        }
+        else {
+          var my_searches = [];
+        }  
+        console.log(my_searches);
+        my_searches.push($('#edit-search-api-fulltext').val());
+        if (my_searches.length > 4) {
+          my_searches.shift();
+        }
+        console.log(my_searches);
+
+        $.cookie('my_searches', JSON.stringify(my_searches), { expires: 999 });
+      });
+
     }
   };
 }(jQuery));
