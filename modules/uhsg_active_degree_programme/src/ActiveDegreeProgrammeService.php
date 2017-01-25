2433b7e73728a3be667dfc3eeefba538aa1e1c11
Mikael Kundert <mikael.kundert@wunderkraut.com>
2017-01-25 15:58:23 +0200

    HUB-73: Created degree programme setter -controller and adapt it from the selector block

--- modules/uhsg_active_degree_programme/src/ActiveDegreeProgrammeService.php
+++ modules/uhsg_active_degree_programme/src/ActiveDegreeProgrammeService.php
@@ -6,13 +6,15 @@
  */
  
 namespace Drupal\uhsg_active_degree_programme;
+
+use Drupal\taxonomy\Entity\Term;
  
 class ActiveDegreeProgrammeService {
 
   /**
    * Set active degree programme.
    */
-  private function set($term) {
+  public function set($term) {
     $tid = $term->id();
     $cookie = array('degree_programme' => $tid);
     user_cookie_save($cookie);
@@ -41,37 +43,18 @@ class ActiveDegreeProgrammeService {
   /**
    * Return term of active degree programme.
    */
-  private function getTerm() {
+  public function getTerm() {
 
-    // if term is in get parameters, it is active.
+    // Primarily active degree programme can be overridden by parameters
     $query_param = \Drupal::Request()->get('degree_programme');
     if ($query_param) {
-      $term = \Drupal\taxonomy\Entity\Term::load($query_param);
-      $this->set($term);
+      $term = Term::load($query_param);
       return $term;
     }
 
-    // if term id is in route parameters, it is active.
-    $param = \Drupal::service('current_route_match')->getParameters()->get('tid');
-    if ($param) {
-      $term = \Drupal\taxonomy\Entity\Term::load($param);
-      $this->set($term);
-      return $term;
-    }
-
-    // If page is degree programme term, get name from there.
-    // This means the user has changed the active degree programme.
-    $term = \Drupal::routeMatch()->getParameter('taxonomy_term');
-    if ($term && $term->bundle() == 'degree_programme') {
-      $this->set($term);
-      return $term;
-    }
-
-    // as a fallback term is set in cookie, lets use that if nothing else works.
-    // This happens on the front page and theme nodes, also on news and article nodes
-    // when tid is not in route parameters.
+    // Secondarily active degree is stored in cookie
     if (isset($_COOKIE['Drupal_visitor_degree_programme'])) {
-      $term = \Drupal\taxonomy\Entity\Term::load($_COOKIE['Drupal_visitor_degree_programme']);
+      $term = Term::load($_COOKIE['Drupal_visitor_degree_programme']);
       return $term;
     }
 
