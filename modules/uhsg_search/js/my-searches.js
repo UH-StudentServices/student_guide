c14a805fda4999daf116df0fbee8b7a9697700be
lviit <lauri.viitala@wunderkraut.com>
2017-01-26 13:47:11 +0200

    HUB-37: we dont need a submit click event handler

--- modules/uhsg_search/js/my-searches.js
+++ modules/uhsg_search/js/my-searches.js
@@ -4,28 +4,11 @@
     attach: function(context, settings) {
 
       var searchInput = $('#views-exposed-form-search-block-1 input[name="search_api_fulltext"]');
-      var searchSubmit = $('#views-exposed-form-search-block-1 .form-submit:not(".button--reset")');
+      var my_searches = $.cookie('my_searches') ? JSON.parse($.cookie('my_searches')) : [];
 
-      //retrieve searches from cookie and display them in list
-      if ($.cookie('my_searches')) {
-        var my_searches = JSON.parse($.cookie('my_searches'));
+      // store submitted value in cookie
+      if (searchInput.val()) {
 
-        var content = '';
-        my_searches.map(function(value) {
-          content += '<li class="list-of-links__link button--action-before icon--search theme-transparent">' + value + '</li>';
-        });
-
-        var title = '<h3>' + Drupal.t('My Searches') + '</h3>';
-        $('#my-searches').empty();
-        $('#my-searches').append(title + '<ul class="list-of-links">' + content + '</ul>');
-        $('#my-searches').append('<button class="button--reset">' + Drupal.t('Remove') + '</button><i class="icon--remove"></i>');
-      }
-      else {
-        my_searches = [];
-      }
-
-      // store submitted search keywords in cookie
-      searchSubmit.on('click', function() {
         // no duplicates
         var dupe = my_searches.find(function(item) {
           return item == searchInput.val() ? true : false;
@@ -37,22 +20,36 @@
           if (my_searches.length > 4) {
             my_searches.pop();
           }
+
+          // store my searches in cookie
+          $.cookie('my_searches', JSON.stringify(my_searches), { expires: 999 });
         }
-        // store my searches in cookie
-        $.cookie('my_searches', JSON.stringify(my_searches), { expires: 999 });
-      });
+      }
 
-      // enable search when clicking one of my searches items
-      $('#my-searches li').on('click', function() {
-        searchInput.val($(this).text());
-        searchSubmit.click();
-      });
+      // display searches in a list
+      if (my_searches.length) {
+        var content = '';
+        my_searches.map(function(value) {
+          content += '<li class="list-of-links__link button--action-before icon--search theme-transparent">' + value + '</li>';
+        });
 
-      // Remove my searches and delete cookie
-      $('.button--reset', '#my-searches').on('click', function() {
-        $.removeCookie('my_searches');
+        var title = '<h3>' + Drupal.t('My Searches') + '</h3>';
         $('#my-searches').empty();
-      });
+        $('#my-searches').append(title + '<ul class="list-of-links">' + content + '</ul>');
+        $('#my-searches').append('<button class="button--reset">' + Drupal.t('Remove') + '</button><i class="icon--remove"></i>');
+
+        // enable search when clicking one of my searches items
+        $('#my-searches li').on('click', function() {
+          searchInput.val($(this).text());
+          searchSubmit.click();
+        });
+
+        // Remove my searches and delete cookie
+        $('.button--reset', '#my-searches').on('click', function() {
+          $.removeCookie('my_searches');
+          $('#my-searches').empty();
+        });
+      }
     }
   };
 }(jQuery));
