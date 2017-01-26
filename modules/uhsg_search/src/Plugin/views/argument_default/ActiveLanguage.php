e116ff76fe96c3beaa0fce1f17472b1367b95f43
lviit <lauri.viitala@wunderkraut.com>
2017-01-26 22:57:11 +0200

    HUB-36: active language as contextual filter so we can use a single view block  for all languages

---
+++ modules/uhsg_search/src/Plugin/views/argument_default/ActiveLanguage.php
@@ -0,0 +1,23 @@
+<?php
+namespace Drupal\uhsg_search\Plugin\views\argument_default;
+use Drupal\views\Plugin\views\argument_default\ArgumentDefaultPluginBase;
+use Drupal\Core\Language\LanguageInterface;
+
+/**
+ * The active degree programme argument default handler.
+ *
+ * @ingroup views_argument_default_plugins
+ *
+ * @ViewsArgumentDefault(
+ *   id = "active_language",
+ *   title = @Translation("Langcode from active language")
+ * )
+ */
+class ActiveLanguage extends ArgumentDefaultPluginBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getArgument() {
+    return \Drupal::languageManager()->getCurrentLanguage()->getId();
+  }
+}
