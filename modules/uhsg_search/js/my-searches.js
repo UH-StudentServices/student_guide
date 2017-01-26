7ab82d3ddafd63a0583c18c04efe9a2010f3ca27
lviit <lauri.viitala@wunderkraut.com>
2017-01-26 12:31:16 +0200

    HUB-37: revert to old jquery/cookie implementation, this plays nicely with drupal

---
+++ modules/uhsg_search/js/my-searches.js
@@ -0,0 +1,63 @@
+(function ($) {
+  'use strict';
+  Drupal.behaviors.uhsg_search_my_searches = {
+    attach: function(context, settings) {
+
+      var searchInput = $('#views-exposed-form-search-block-1 input[name="search_api_fulltext"]');
+      var searchSubmit = $('#views-exposed-form-search-block-1 .form-submit:not(".button--reset")');
+
+      //retrieve searches from cookie and display them in list
+      if ($.cookie('my_searches')) {
+        var my_searches = JSON.parse($.cookie('my_searches'));
+        my_searches.reverse();
+
+        var content = '';
+        my_searches.map(function(value) {
+          content += '<li class="list-of-links__link button--action-before icon--search theme-transparent">' + value + '</li>';
+        });
+        console.log(content);
+        var title = '<h3>' + Drupal.t('My Searches') + '</h3>';
+        $('#my-searches').empty();
+        $('#my-searches').append(title + '<ul class="list-of-links">' + content + '</ul>');
+        $('#my-searches').append('<button class="button--reset">' + Drupal.t('Remove') + '</button><i class="icon--remove"></i>');
+      }
+
+      // store submitted search keywords in cookie
+      searchSubmit.on('click', function() {
+        if ($.cookie('my_searches')) {
+          var my_searches = JSON.parse($.cookie('my_searches'));
+        }
+        else {
+          var my_searches = [];
+        }
+
+        // no duplicates
+        var dupe = my_searches.find(function(item) {
+          return item == searchInput.val() ? true : false;
+        });
+
+        if (!dupe) {
+          my_searches.push(searchInput.val());
+          // store only 4 latest searches
+          if (my_searches.length > 4) {
+            my_searches.shift();
+          }
+        }
+        // store my searches in cookie
+        $.cookie('my_searches', JSON.stringify(my_searches), { expires: 999 });
+      });
+
+      // enable search when clicking one of my searches items
+      $('#my-searches li').on('click', function() {
+        searchInput.val($(this).text());
+        searchSubmit.click();
+      });
+
+      // Remove my searches and delete cookie
+      $('.button--reset', '#my-searches').on('click', function() {
+        $.removeCookie('my_searches');
+        $('#my-searches').empty();
+      });
+    }
+  };
+}(jQuery));
