21cb0ea9e9efe96877f4983d137e6fb0cab20d2f
Mikael Kundert <mikael.kundert@wunderkraut.com>
2017-01-27 16:51:43 +0200

    HUB-73: Use service for detecting active degree programme

--- modules/uhsg_active_degree_programme/src/PageCache/DenyActiveDegreeProgramme.php
+++ modules/uhsg_active_degree_programme/src/PageCache/DenyActiveDegreeProgramme.php
@@ -3,18 +3,29 @@
 namespace Drupal\uhsg_active_degree_programme\PageCache;
 
 use Drupal\Core\PageCache\ResponsePolicyInterface;
+use Drupal\uhsg_active_degree_programme\ActiveDegreeProgrammeService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DenyActiveDegreeProgramme implements ResponsePolicyInterface {
 
   /**
+   * Service that holds information about active degree programme.
+   *
+   * @var ActiveDegreeProgrammeService
+   */
+  protected $activeDegreeProgrammeService;
+
+  public function __construct(ActiveDegreeProgrammeService $activeDegreeProgrammeService) {
+    $this->activeDegreeProgrammeService = $activeDegreeProgrammeService;
+  }
+
+  /**
    * @inheritdoc
    */
   public function check(Response $response, Request $request) {
-    // If we find degree programme values either from cookies or headers, do not
-    // allow page caching.
-    if ($request->cookies->get('Drupal_visitor_degree_programme') || $request->headers->get('x-degree-programme')) {
+    // If we find degree programme, do not allow page caching.
+    if ($this->activeDegreeProgrammeService->getId()) {
       return self::DENY;
     }
   }
