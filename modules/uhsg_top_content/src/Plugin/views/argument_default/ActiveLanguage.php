06f95255036b2be3103c0421e32960d0555c793b
lviit <lauri.viitala@wunderkraut.com>
2017-01-27 11:23:04 +0200

    HUB-36: add language interface cache context to custom plugin. This is always added by views, but we want to be extra sure.

--- modules/uhsg_top_content/src/Plugin/views/argument_default/ActiveLanguage.php
+++ modules/uhsg_top_content/src/Plugin/views/argument_default/ActiveLanguage.php
@@ -1,6 +1,7 @@
 <?php
 namespace Drupal\uhsg_top_content\Plugin\views\argument_default;
 use Drupal\views\Plugin\views\argument_default\ArgumentDefaultPluginBase;
+use Drupal\Core\Cache\CacheableDependencyInterface;
 use Drupal\Core\Language\LanguageInterface;
 
 /**
@@ -13,11 +14,26 @@ use Drupal\Core\Language\LanguageInterface;
  *   title = @Translation("Langcode from active language")
  * )
  */
-class ActiveLanguage extends ArgumentDefaultPluginBase {
+class ActiveLanguage extends ArgumentDefaultPluginBase implements CacheableDependencyInterface {
   /**
    * {@inheritdoc}
    */
   public function getArgument() {
     return \Drupal::languageManager()->getCurrentLanguage()->getId();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheMaxAge() {
+    return 0;
+  }
+
+  /**
+   * {@inheritdoc}
+   * This is always added by views but we are adding it here just in case.
+   */
+  public function getCacheContexts() {
+    return ['languages:language_interface'];
   }
 }
