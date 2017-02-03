84f86482a8052984de4d775143203bb0f916e2e6
lviit <lauri.viitala@wunderkraut.com>
2017-02-03 11:54:24 +0200

    HUB-100: focus on search input when opening switcher

--- themes/uhsg_theme/js/degree_programme_switcher.js
+++ themes/uhsg_theme/js/degree_programme_switcher.js
@@ -2,9 +2,11 @@
   'use strict';
   Drupal.behaviors.degreeProgrammeSwitcher = {
     attach: function(context, settings) {
-      var header = $('.degree-programme-switcher__header'),
-          container = $('.degree-programme-switcher__container'),
-          toggle = $('.degree-programme-switcher__toggle'),
+      var degreeProgrammeSwitcher = '.degree-programme-switcher',
+          header = $('.degree-programme-switcher__header', degreeProgrammeSwitcher),
+          container = $('.degree-programme-switcher__container', degreeProgrammeSwitcher),
+          toggle = $('.degree-programme-switcher__toggle', degreeProgrammeSwitcher),
+          searchInput = $('input[name="name"]', degreeProgrammeSwitcher),
           toggleClass = 'collapsed',
           toggleTextOpen = Drupal.t('Change'),
           toggleTextClosed = Drupal.t('Close');
@@ -12,6 +14,7 @@
       header.once().on('click', function() {
         container.toggleClass(toggleClass);
         toggle.text(container.hasClass(toggleClass) ? toggleTextOpen : toggleTextClosed);
+        searchInput.focus();
       });
     }
   };
