36560b57cf65dab060085319e3b96bfe1ee9858e
Mikael Kundert <mikael.kundert@wunderkraut.com>
2017-02-08 10:24:18 +0200

    HUB-96: Replace controllers in route altering for all overridden methods

--- modules/uhsg_samlauth/src/Routing/RouteSubscriber.php
+++ modules/uhsg_samlauth/src/Routing/RouteSubscriber.php
@@ -11,8 +11,16 @@ class RouteSubscriber extends RouteSubscriberBase {
    */
   public function alterRoutes(RouteCollection $collection) {
     // Replace saml login with own implementation that has additional features.
-    if ($route = $collection->get('samlauth.saml_controller_login')) {
-      $route->setDefault('_controller', 'Drupal\\uhsg_samlauth\\Controller\\SamlController::login');
+    $controller_replacements = [
+      'samlauth.saml_controller_login' => 'Drupal\\uhsg_samlauth\\Controller\\SamlController::login',
+      'samlauth.saml_controller_logout' => 'Drupal\\uhsg_samlauth\\Controller\\SamlController::logout',
+      'samlauth.saml_controller_acs' => 'Drupal\\uhsg_samlauth\\Controller\\SamlController::acs',
+      'samlauth.saml_controller_sls' => 'Drupal\\uhsg_samlauth\\Controller\\SamlController::sls',
+    ];
+    foreach ($controller_replacements as $route_name => $controller) {
+      if ($route = $collection->get($route_name)) {
+        $route->setDefault('_controller', $controller);
+      }
     }
   }
 }
