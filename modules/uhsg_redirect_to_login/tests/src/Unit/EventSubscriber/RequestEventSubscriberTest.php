92d03b7d117f483ebbbc93e027e8eace0ddd981a
Jani Palsamäki <jani.palsamaki@gmail.com>
2017-02-13 08:29:32 +0200

    RequestEventSubscriber: Added a unit test: onRequestShouldNotRespondToEventWhenCookiesDoNotExist

--- modules/uhsg_redirect_to_login/tests/src/Unit/EventSubscriber/RequestEventSubscriberTest.php
+++ modules/uhsg_redirect_to_login/tests/src/Unit/EventSubscriber/RequestEventSubscriberTest.php
@@ -2,6 +2,9 @@
 
 use Drupal\Tests\UnitTestCase;
 use Drupal\uhsg_redirect_to_login\EventSubscriber\RequestEventSubscriber;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
@@ -9,11 +12,28 @@ use Symfony\Component\HttpKernel\KernelEvents;
  */
 class RequestEventSubscriberTest extends UnitTestCase {
 
+  /** @var ParameterBag */
+  private $cookies;
+
+  /** @var GetResponseEvent */
+  private $event;
+
+  /** @var Request */
+  private $request;
+
   /** @var RequestEventSubscriber */
   private $requestEventSubscriber;
 
   public function setUp() {
     parent::setUp();
+
+    $this->cookies = $this->prophesize(ParameterBag::class);
+
+    $this->request = $this->prophesize(Request::class);
+    $this->request->cookies = $this->cookies;
+
+    $this->event = $this->prophesize(GetResponseEvent::class);
+    $this->event->getRequest()->willReturn($this->request);
 
     $this->requestEventSubscriber = new RequestEventSubscriber();
   }
@@ -25,5 +45,14 @@ class RequestEventSubscriberTest extends UnitTestCase {
     $events = $this->requestEventSubscriber->getSubscribedEvents();
 
     $this->assertEquals(['onRequest'], $events[KernelEvents::REQUEST][0]);
+  }
+
+  /**
+   * @test
+   */
+  public function onRequestShouldNotRespondToEventWhenCookiesDoNotExist() {
+    $this->event->setResponse()->shouldNotBeCalled();
+
+    $this->requestEventSubscriber->onRequest($this->event->reveal());
   }
 }
