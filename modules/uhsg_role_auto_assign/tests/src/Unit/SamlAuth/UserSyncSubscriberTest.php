32fdf255668a00c8d127f387a63151e2211918be
Jani Palsamäki <jani.palsamaki@gmail.com>
2017-02-16 13:24:12 +0200

    UserSyncSubscriber: Added a unit tests: shouldAssignRoleToUserWhenTheUserBelongsToAssignableGroup, shouldUnassignRoleFromUserWhenTheUserDoesNotBelongToAssignableGroup

--- modules/uhsg_role_auto_assign/tests/src/Unit/SamlAuth/UserSyncSubscriberTest.php
+++ modules/uhsg_role_auto_assign/tests/src/Unit/SamlAuth/UserSyncSubscriberTest.php
@@ -1,10 +1,18 @@
 <?php
 
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\ImmutableConfig;
+use Drupal\Core\Entity\EntityManager;
+use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Logger\LoggerChannelFactory;
 use Drupal\Tests\UnitTestCase;
 use Drupal\samlauth\Event\SamlAuthEvents;
+use Drupal\samlauth\Event\SamlAuthUserSyncEvent;
 use Drupal\uhsg_role_auto_assign\SamlAuth\UserSyncSubscriber;
+use Drupal\user\Entity\Role;
+use Drupal\user\UserInterface;
+use Prophecy\Argument;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -12,14 +20,41 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class UserSyncSubscriberTest extends UnitTestCase {
 
+  const GROUP_1_NAME = 'Group 1 name';
+  const GROUP_2_NAME = 'Group 2 name';
+  const ROLE_1_ID = 1;
+  const ROLE_2_ID = 2;
+  const USER_NAME = 'User name';
+
+  /** @var UserInterface */
+  private $account;
+
   /** @var ConfigFactoryInterface */
   private $configFactory;
+
+  /** @var ImmutableConfig */
+  private $config;
 
   /** @var ContainerInterface */
   private $container;
 
+  /** @var EntityManager */
+  private $entityManager;
+
+  /** @var EntityStorageInterface */
+  private $entityStorage;
+
+  /** @var SamlAuthUserSyncEvent */
+  private $event;
+
+  /** @var LoggerInterface */
+  private $logger;
+
   /** @var LoggerChannelFactory */
   private $loggerChannelFactory;
+
+  /** @var Role */
+  private $role;
 
   /** @var UserSyncSubscriber */
   private $userSyncSubscriber;
@@ -27,11 +62,38 @@ class UserSyncSubscriberTest extends UnitTestCase {
   public function setUp() {
     parent::setUp();
 
+    $this->account = $this->prophesize(UserInterface::class);
+    $this->account->hasRole(Argument::any())->willReturn(FALSE);
+    $this->account->label()->willReturn(self::USER_NAME);
+
+    $this->config = $this->prophesize(ImmutableConfig::class);
+    $this->config->get('group_to_roles')->willReturn([['rid' => self::ROLE_1_ID, 'group_name' => self::GROUP_1_NAME]]);
+
     $this->configFactory = $this->prophesize(ConfigFactoryInterface::class);
+    $this->configFactory->get('uhsg_role_auto_assign.settings')->willReturn($this->config);
+
+    $this->role = $this->prophesize(Role::class);
+    $this->role->id()->willReturn(self::ROLE_1_ID);
+
+    $this->entityStorage = $this->prophesize(EntityStorageInterface::class);
+    $this->entityStorage->load(Argument::any())->willReturn($this->role);
+
+    $this->entityManager = $this->prophesize(EntityManager::class);
+    $this->entityManager->getEntityTypeFromClass(Argument::any())->willReturn('');
+    $this->entityManager->getStorage(Argument::any())->willReturn($this->entityStorage);
+
+    $this->event = $this->prophesize(SamlAuthUserSyncEvent::class);
+    $this->event->getAccount()->willReturn($this->account);
+    $this->event->getAttributes()->willReturn(['urn:mace:funet.fi:helsinki.fi:hyGroupCn' => [self::GROUP_1_NAME]]);
+
+    $this->logger = $this->prophesize(LoggerInterface::class);
+
     $this->loggerChannelFactory = $this->prophesize(LoggerChannelFactory::class);
+    $this->loggerChannelFactory->get(Argument::any())->willReturn($this->logger);
 
     $this->container = $this->prophesize(ContainerInterface::class);
     $this->container->get('config.factory')->willReturn($this->configFactory);
+    $this->container->get('entity.manager')->willReturn($this->entityManager);
     $this->container->get('logger.factory')->willReturn($this->loggerChannelFactory);
 
     \Drupal::setContainer($this->container->reveal());
@@ -46,5 +108,28 @@ class UserSyncSubscriberTest extends UnitTestCase {
     $events = $this->userSyncSubscriber->getSubscribedEvents();
 
     $this->assertEquals('onUserSync', $events[SamlAuthEvents::USER_SYNC][0][0]);
+  }
+
+  /**
+   * @test
+   */
+  public function shouldAssignRoleToUserWhenTheUserBelongsToAssignableGroup() {
+    $this->account->addRole(Argument::any())->shouldBeCalled();
+    $this->event->markAccountChanged()->shouldBeCalled();
+
+    $this->userSyncSubscriber->onUserSync($this->event->reveal());
+  }
+
+  /**
+   * @test
+   */
+  public function shouldUnassignRoleFromUserWhenTheUserDoesNotBelongToAssignableGroup() {
+    $this->account->hasRole(Argument::any())->willReturn(TRUE);
+    $this->event->getAttributes()->willReturn(['urn:mace:funet.fi:helsinki.fi:hyGroupCn' => [self::GROUP_2_NAME]]);
+
+    $this->account->removeRole(Argument::any())->shouldBeCalled();
+    $this->event->markAccountChanged()->shouldBeCalled();
+
+    $this->userSyncSubscriber->onUserSync($this->event->reveal());
   }
 }
