43b86d1430d11533f47b33df81abe168c6f65cca
Mikael Kundert <mikael.kundert@wunderkraut.com>
2017-02-27 14:59:28 +0200

    HUB-56: Should deny flagging/unflagging access if its marked as technically managed

---
+++ modules/uhsg_degree_programme/src/Flag/MyDegreeProgrammeFlagType.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\uhsg_degree_programme\Flag;
+
+use Drupal\Core\Access\AccessResultForbidden;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\flag\FlagInterface;
+use Drupal\flag\FlagServiceInterface;
+use Drupal\flag\Plugin\Flag\EntityFlagType;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * This class overrides EntityFlagType so that we can override the actionAccess
+ * to follow business logic we want.
+ */
+class MyDegreeProgrammeFlagType extends EntityFlagType {
+
+  /**
+   * Specify the flag id where we want to apply this specific business logic.
+   * @var string
+   */
+  protected $apply_custom_access_logic_id = 'my_degree_programmes';
+
+  /**
+   * @var FlagServiceInterface
+   */
+  protected $flagService;
+
+  /**
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $config;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, array $plugin_definition, ModuleHandlerInterface $module_handler, EntityTypeManagerInterface $entity_type_manager, FlagServiceInterface $flagService, ConfigFactoryInterface $configFactory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $module_handler, $entity_type_manager);
+    $this->flagService = $flagService;
+    $this->config = $configFactory->get('uhsg_degree_programme.settings');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('module_handler'),
+      $container->get('entity_type.manager'),
+      $container->get('flag'),
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function actionAccess($action, FlagInterface $flag, AccountInterface $account, EntityInterface $flaggable = NULL) {
+    $access = parent::actionAccess($action, $flag, $account, $flaggable);
+
+    // When specified flag type, then introduce some additional access logic
+    if ($flag->id() == $this->apply_custom_access_logic_id && !is_null($flaggable)) {
+      // Get the flagging and deny access if the flagging has been created
+      // programmatically.
+      if ($flagging = $this->flagService->getFlagging($flag, $flaggable, $account)) {
+        if ($flagging->hasField($this->config->get('technical_condition_field_name')) &&
+            !$flagging->get($this->config->get('technical_condition_field_name'))->isEmpty() &&
+            $flagging->get($this->config->get('technical_condition_field_name'))->first()->getValue()['value']) {
+          $access = $access->andIf(new AccessResultForbidden());
+        }
+      }
+    }
+
+    return $access;
+  }
+}
