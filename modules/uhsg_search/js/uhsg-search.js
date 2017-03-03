3357c2b77e183949f129e4b65cce3160f362413f
lviit <lauri.viitala@wunderkraut.com>
2017-03-03 16:01:31 +0200

    HUB-136: fix autosubmit

--- modules/uhsg_search/js/uhsg-search.js
+++ modules/uhsg_search/js/uhsg-search.js
@@ -1,16 +1,10 @@
 (function ($) {
-  'use strict';
-  Drupal.behaviors.uhsg_search_reset = {
-    attach: function(context, settings) {
-
-      // Exposed filter reset redirects user to 404 page on AJAX view when
-      // placed as a block (https://www.drupal.org/node/2820347). Until core is
-      // fixed, using location.reload() to reload the current page to reset the
-      // form.
-      $('.view-search .views-exposed-form input.button--reset').click(function (e) {
-        e.preventDefault();
-    	  location.reload();
-      })
+  // Override autocomplete select
+  var oldSelect = Drupal.autocomplete.options.select;
+  Drupal.autocomplete.options.select = function(event, ui) {
+    oldSelect.call(this, event, ui);
+    if ($(this).hasClass('ui-autocomplete-input')) {
+      $(this).siblings('.form-actions').children('#edit-submit-search').trigger('click');
     }
   };
 }(jQuery));
