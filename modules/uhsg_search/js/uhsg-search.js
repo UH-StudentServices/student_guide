20201807d9c0f0685471605ffd369da6575fd8c2
lviit <lauri.viitala@wunderkraut.com>
2017-03-06 12:31:37 +0200

    HUB-136: fix two clicks needed for search suggestion on ios

--- modules/uhsg_search/js/uhsg-search.js
+++ modules/uhsg_search/js/uhsg-search.js
@@ -1,10 +1,15 @@
 (function ($) {
-  // Override autocomplete select
+  // Submit search when clicking suggestions
   var oldSelect = Drupal.autocomplete.options.select;
   Drupal.autocomplete.options.select = function(event, ui) {
     oldSelect.call(this, event, ui);
     if ($(this).hasClass('ui-autocomplete-input')) {
       $(this).siblings('.form-actions').children('#edit-submit-search').trigger('click');
     }
+  };
+  // autocomlete requires the user to click twice in iOS, fix that
+  Drupal.autocomplete.options.open = function (event, ui) {
+    console.log('open jepajee.');
+    $('.ui-autocomplete').off('menufocus hover mouseover');
   };
 }(jQuery));
