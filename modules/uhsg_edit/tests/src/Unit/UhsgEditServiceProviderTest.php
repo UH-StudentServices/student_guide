a7aa2a0bd68e58158594aadad4971121f306237b
Jani Palsamäki <jani.palsamaki@gmail.com>
2017-03-06 08:18:13 +0200

    UhsgEditServiceProvider: Added a unit test: shouldReplaceOriginalContentLockWithCustomOne

---
+++ modules/uhsg_edit/tests/src/Unit/UhsgEditServiceProviderTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Tests\UnitTestCase;
+use Drupal\uhsg_edit\UhsgEditServiceProvider;
+use Symfony\Component\DependencyInjection\Definition;
+
+/**
+ * @group uhsg
+ */
+class UhsgEditServiceProviderTest extends UnitTestCase {
+
+  /** @var ContainerBuilder */
+  private $container;
+
+  /** @var Definition */
+  private $definition;
+
+  /** @var UhsgEditServiceProvider */
+  private $uhsgEditServiceProvider;
+
+  public function setUp() {
+    parent::setUp();
+
+    $this->definition = $this->prophesize(Definition::class);
+
+    $this->container = $this->prophesize(ContainerBuilder::class);
+    $this->container->hasDefinition('content_lock')->willReturn(TRUE);
+    $this->container->getDefinition('content_lock')->willReturn($this->definition);
+
+    $this->uhsgEditServiceProvider = new UhsgEditServiceProvider();
+  }
+
+  /**
+   * @test
+   */
+  public function shouldReplaceOriginalContentLockWithCustomOne() {
+    $this->definition->setClass('Drupal\uhsg_edit\ContentLock\UhsgContentLock')->shouldBeCalled();
+
+    $this->uhsgEditServiceProvider->alter($this->container->reveal());
+  }
+}
