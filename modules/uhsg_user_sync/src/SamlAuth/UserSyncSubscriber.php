22643a4fc53b9393e05cc4308d21e88a697befef
Jani Palsamäki <jani.palsamaki@gmail.com>
2017-03-08 14:52:26 +0200

    Temp: Debugging login attributes.

--- modules/uhsg_user_sync/src/SamlAuth/UserSyncSubscriber.php
+++ modules/uhsg_user_sync/src/SamlAuth/UserSyncSubscriber.php
@@ -66,6 +66,7 @@ class UserSyncSubscriber implements EventSubscriberInterface {
 
   public function onUserSync(SamlAuthUserSyncEvent $event) {
     $attributes = new AttributeParser($event->getAttributes());
+    $this->logger->debug(print_r($attributes, TRUE));
     $this->syncStudentID($event, $attributes);
 
     try {
