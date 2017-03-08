f841b0c9eeda39257d0b9edf7fce02aed6befea7
lviit <lauri.viitala@wunderkraut.com>
2017-03-08 12:14:16 +0200

    HUB-130: fix indentation

--- modules/uhsg_avatar/src/AvatarService.php
+++ modules/uhsg_avatar/src/AvatarService.php
@@ -11,26 +11,26 @@ use GuzzleHttp\Client;
 class AvatarService {
 
   private function apiUrl($oodi_uid) {
-	  $api_base_url = 'https://student.helsinki.fi/';
-	  $api_path = "api/public/v1/profile/$oodi_uid";
-	  return $api_base_url . $api_path;
+    $api_base_url = 'https://student.helsinki.fi/';
+    $api_path = "api/public/v1/profile/$oodi_uid";
+    return $api_base_url . $api_path;
   }
 
   /**
    * Fetch avatar.
    */
   public function fetchAvatar($oodi_uid) {
-	  $api_url = $this->apiUrl($oodi_uid);
+    $api_url = $this->apiUrl($oodi_uid);
 
-	  if ($api_url) {
-	  	$client = new \GuzzleHttp\Client();
+    if ($api_url) {
+      $client = new \GuzzleHttp\Client();
       $api_response = $client->get($api_url, ['http_errors' => false]);
 
       if ($api_response->getStatusCode() == 200) {
-      	$body = $api_response->getBody();
-      	$obj = json_decode($body);
-      	return $obj->avatarImageUrl;
+        $body = $api_response->getBody();
+        $obj = json_decode($body);
+        return $obj->avatarImageUrl;
       }
-	  }
+    }
   }
 }
