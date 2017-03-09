5fe30114c77f0139b8579f06fc75ef31157c8b86
Jani Palsamäki <jani.palsamaki@gmail.com>
2017-03-09 09:06:18 +0200

    UserSynscSubscriber: Added a unit test: onUserSyncShouldSyncStudentIdWhenItHasChanged

--- modules/uhsg_user_sync/tests/src/Unit/SamlAuth/UserSyncSubscriberTest.php
+++ modules/uhsg_user_sync/tests/src/Unit/SamlAuth/UserSyncSubscriberTest.php
@@ -3,20 +3,26 @@
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Config\ImmutableConfig;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Logger\LoggerChannel;
 use Drupal\flag\FlagServiceInterface;
-use Drupal\Tests\UnitTestCase;
 use Drupal\samlauth\Event\SamlAuthEvents;
 use Drupal\samlauth\Event\SamlAuthUserSyncEvent;
+use Drupal\Tests\UnitTestCase;
 use Drupal\uhsg_oprek\Oprek\OprekServiceInterface;
 use Drupal\uhsg_user_sync\SamlAuth\UserSyncSubscriber;
+use Drupal\user\UserInterface;
+use Prophecy\Argument;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * @group uhsg
  */
 class UserSynscSubscriberTest extends UnitTestCase {
 
-  const ATTRIBUTES = [];
+  const ATTRIBUTES = ['studentId'];
+  const STUDENT_ID = '123';
   const STUDENT_ID_FIELD_CONFIG_KEY = 'studentID_field_name';
   const STUDENT_ID_FIELD_CONFIG_VALUE = 'field_student_id';
 
@@ -26,11 +32,20 @@ class UserSynscSubscriberTest extends UnitTestCase {
   /** @var ImmutableConfig */
   private $config;
 
+  /** @var ContainerInterface */
+  private $container;
+
   /** @var EntityTypeManagerInterface */
   private $entityTypeManager;
 
   /** @var SamlAuthUserSyncEvent */
   private $event;
+
+  /** @var FieldDefinitionInterface */
+  private $fieldDefinition;
+
+  /** @var FieldItemListInterface */
+  private $fieldItemList;
 
   /** @var FlagServiceInterface */
   private $flagService;
@@ -40,6 +55,9 @@ class UserSynscSubscriberTest extends UnitTestCase {
 
   /** @var OprekServiceInterface */
   private $oprekService;
+
+  /** @var UserInterface */
+  private $user;
 
   /** @var UserSyncSubscriber */
   private $userSyncSubscriber;
@@ -55,12 +73,26 @@ class UserSynscSubscriberTest extends UnitTestCase {
 
     $this->entityTypeManager = $this->prophesize(EntityTypeManagerInterface::class);
 
+    $this->fieldDefinition = $this->prophesize(FieldDefinitionInterface::class);
+
+    $this->fieldItemList = $this->prophesize(FieldItemListInterface::class);
+    $this->fieldItemList->getString()->willReturn(self::STUDENT_ID);
+
+    $this->user = $this->prophesize(UserInterface::class);
+    $this->user->getFieldDefinition(self::STUDENT_ID_FIELD_CONFIG_VALUE)->willReturn($this->fieldDefinition);
+    $this->user->get(self::STUDENT_ID_FIELD_CONFIG_VALUE)->willReturn($this->fieldItemList);
+
     $this->event = $this->prophesize(SamlAuthUserSyncEvent::class);
+    $this->event->getAccount()->willReturn($this->user);
     $this->event->getAttributes()->willReturn(self::ATTRIBUTES);
 
     $this->flagService = $this->prophesize(FlagServiceInterface::class);
     $this->logger = $this->prophesize(LoggerChannel::class);
     $this->oprekService = $this->prophesize(OprekServiceInterface::class);
+
+    $this->container = $this->prophesize(ContainerInterface::class);
+
+    Drupal::setContainer($this->container->reveal());
 
     $this->userSyncSubscriber = new UserSyncSubscriber(
       $this->configFactory->reveal(),
@@ -87,6 +119,16 @@ class UserSynscSubscriberTest extends UnitTestCase {
     $this->config->get(self::STUDENT_ID_FIELD_CONFIG_KEY)->willReturn(NULL);
 
     $this->event->getAccount()->shouldNotBeCalled();
+
+    $this->userSyncSubscriber->onUserSync($this->event->reveal());
+  }
+
+  /**
+   * @test
+   */
+  public function onUserSyncShouldSyncStudentIdWhenItHasChanged() {
+    $this->fieldItemList->setValue(Argument::any())->shouldBeCalled();
+    $this->event->markAccountChanged()->shouldBeCalled();
 
     $this->userSyncSubscriber->onUserSync($this->event->reveal());
   }
