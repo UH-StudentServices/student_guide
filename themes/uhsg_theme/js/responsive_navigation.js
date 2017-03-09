3cdfbe8dbad15c8197eec1cb5e9b1ec25ec4980b
lviit <lauri.viitala@wunderkraut.com>
2017-03-09 10:35:03 +0200

    HUB-130: move avatar when in mobile

---
+++ themes/uhsg_theme/js/responsive_navigation.js
@@ -0,0 +1,33 @@
+(function ($) {
+  'use strict';
+  Drupal.behaviors.responsive_navigation = {
+    attach: function(context, settings) {
+      var mainUl = $('.main-menu > ul'),
+          menuToggle = $('#menu-toggle'),
+          breakpoints = settings.breakpoints,
+          avatar = $('.main-menu__avatar'),
+          avatarMobileMenu = $('.block-language .links'),
+          avatarDesktopMenu = $('.main-menu .menu');
+
+      // toggle mobile menu
+      menuToggle.once().on('click', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('active');
+        mainUl.toggleClass('is-open').toggleClass('is-slidein');
+      });
+
+      // Check media queries and add listener
+      if (typeof matchMedia !== 'undefined') {
+        var mq = window.matchMedia(breakpoints['small']);
+        mq.addListener(moveAvatar);
+        moveAvatar(mq);
+      }
+
+      // Move avatar if media query matches
+      function moveAvatar(mq) {
+        mq.matches ? avatarDesktopMenu.append(avatar) : avatarMobileMenu.append(avatar);  
+      }
+
+    }
+  };
+}(jQuery));
