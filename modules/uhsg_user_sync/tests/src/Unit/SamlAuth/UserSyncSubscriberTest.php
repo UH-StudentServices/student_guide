6e135884c309403c6eca238b33211d4a851d3991
Jani Palsamäki <jani.palsamaki@gmail.com>
2017-03-09 08:22:59 +0200

    UserSynscSubscriber: Added a unit test: onUserSyncShouldDoNothingIfStudentIdFieldNameConfigIsMissing

--- modules/uhsg_user_sync/tests/src/Unit/SamlAuth/UserSyncSubscriberTest.php
+++ modules/uhsg_user_sync/tests/src/Unit/SamlAuth/UserSyncSubscriberTest.php
@@ -1,11 +1,13 @@
 <?php
 
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\ImmutableConfig;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Logger\LoggerChannel;
 use Drupal\flag\FlagServiceInterface;
 use Drupal\Tests\UnitTestCase;
 use Drupal\samlauth\Event\SamlAuthEvents;
+use Drupal\samlauth\Event\SamlAuthUserSyncEvent;
 use Drupal\uhsg_oprek\Oprek\OprekServiceInterface;
 use Drupal\uhsg_user_sync\SamlAuth\UserSyncSubscriber;
 
@@ -14,11 +16,21 @@ use Drupal\uhsg_user_sync\SamlAuth\UserSyncSubscriber;
  */
 class UserSynscSubscriberTest extends UnitTestCase {
 
+  const ATTRIBUTES = [];
+  const STUDENT_ID_FIELD_CONFIG_KEY = 'studentID_field_name';
+  const STUDENT_ID_FIELD_CONFIG_VALUE = 'field_student_id';
+
   /** @var ConfigFactoryInterface */
   private $configFactory;
 
+  /** @var ImmutableConfig */
+  private $config;
+
   /** @var EntityTypeManagerInterface */
   private $entityTypeManager;
+
+  /** @var SamlAuthUserSyncEvent */
+  private $event;
 
   /** @var FlagServiceInterface */
   private $flagService;
@@ -35,8 +47,17 @@ class UserSynscSubscriberTest extends UnitTestCase {
   public function setUp() {
     parent::setUp();
 
+    $this->config = $this->prophesize(ImmutableConfig::class);
+    $this->config->get(self::STUDENT_ID_FIELD_CONFIG_KEY)->willReturn(self::STUDENT_ID_FIELD_CONFIG_VALUE);
+
     $this->configFactory = $this->prophesize(ConfigFactoryInterface::class);
+    $this->configFactory->get('uhsg_user_sync.settings')->willReturn($this->config);
+
     $this->entityTypeManager = $this->prophesize(EntityTypeManagerInterface::class);
+
+    $this->event = $this->prophesize(SamlAuthUserSyncEvent::class);
+    $this->event->getAttributes()->willReturn(self::ATTRIBUTES);
+
     $this->flagService = $this->prophesize(FlagServiceInterface::class);
     $this->logger = $this->prophesize(LoggerChannel::class);
     $this->oprekService = $this->prophesize(OprekServiceInterface::class);
@@ -57,5 +78,16 @@ class UserSynscSubscriberTest extends UnitTestCase {
     $events = $this->userSyncSubscriber->getSubscribedEvents();
 
     $this->assertEquals(['onUserSync'], $events[SamlAuthEvents::USER_SYNC][0]);
+  }
+
+  /**
+   * @test
+   */
+  public function onUserSyncShouldDoNothingIfStudentIdFieldNameConfigIsMissing() {
+    $this->config->get(self::STUDENT_ID_FIELD_CONFIG_KEY)->willReturn(NULL);
+
+    $this->event->getAccount()->shouldNotBeCalled();
+
+    $this->userSyncSubscriber->onUserSync($this->event->reveal());
   }
 }
