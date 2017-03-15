b833bad845007c43e5a34f1b795b5a1ece346fa6
lviit <lauri.viitala@wunderkraut.com>
2017-03-15 15:45:40 +0200

    HUB-173: all button

--- modules/uhsg_search/js/filter_search.js
+++ modules/uhsg_search/js/filter_search.js
@@ -7,7 +7,7 @@
           numResults = $('.view-result', view),
           filterOptions = [];
 
-      numResults.after('<div id="search-filters" class="button-group"></div>');
+      numResults.after('<div id="search-filters" class="button-group"><div class="button-group__button"><a class="button--small" href="#" data-type="all">' + Drupal.t('All') + '</a></div></div>');
       
       results.each(function() {
         var type = $(this).attr('data-type');
@@ -18,21 +18,21 @@
 
         if (!dupe) {
           filterOptions.push(type);
-          $('#search-filters', view).append('<div class="button-group__button"><a class="button--small" href="#">' + Drupal.t(type) + '</a></div>');
+          $('#search-filters', view).append('<div class="button-group__button"><a class="button--small" href="#" data-type="' + type + '">' + Drupal.t(type) + '</a></div>');
         }
       });
 
       $('a', '#search-filters').on('click', function(e) {
         e.preventDefault();
-        var filterType = $(this).text();
-        $(this).toggleClass('is-active');
+        var filterType = $(this).attr('data-type');
+        $(this).addClass('is-active');
         $(this).parent().siblings().children().removeClass('is-active');
         results.each(function() {
-          if ($(this).attr('data-type') != filterType) {
-            $(this).hide();
+          if ($(this).attr('data-type') == filterType || filterType == 'all') {
+            $(this).show();
           }
           else {
-            $(this).show();
+            $(this).hide();
           }
         });
       });
