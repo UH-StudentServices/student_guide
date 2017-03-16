a975f7c0151cfe44999ddde272012c05d081e7ce
lviit <lauri.viitala@wunderkraut.com>
2017-03-16 15:16:51 +0200

    HUB-173: refactor js

--- modules/uhsg_search/js/filter_search.js
+++ modules/uhsg_search/js/filter_search.js
@@ -2,34 +2,63 @@
   'use strict';
   Drupal.behaviors.uhsg_search_filter_search = {
     attach: function(context, settings) {
-      var view = '.view-search',
-          results = $('.view-content article', view),
-          numResults = $('.view-before-content h3', view).after('<div id="search-filters" class="button-group is-center-mobile"></div>'),
-          filterTitles = {
-            'all': Drupal.t('All', {}, {context: 'Search Filters'}),
-            'article_general': Drupal.t('General instructions', {}, {context: 'Search Filters'}),
-            'article_degree_programme_specific': Drupal.t('Degree programme specific instructions', {}, {context: 'Search Filters'}),
-            'theme': Drupal.t('Theme', {}, {context: 'Search Filters'}),
-            'news': Drupal.t('News', {}, {context: 'Search Filters'})
-          },
-          filterButtons = $('#search-filters', view).append(createFilterButton('all', 'is-active')),
-          filterOptions = [];
-      
-      // create buttons for available filter types
+      var filter = this;
+
+      $('.view-search').once().each(function() {
+        var results = $('.view-content article', this),
+            resultCount = $('.view-before-content h3', this).after('<div id="search-filters" class="button-group is-center-mobile"></div>'),
+            filterTitles = {
+              'all': Drupal.t('All', {}, {context: 'Search Filters'}),
+              'article_general': Drupal.t('General instructions', {}, {context: 'Search Filters'}),
+              'article_degree_programme_specific': Drupal.t('Degree programme specific instructions', {}, {context: 'Search Filters'}),
+              'theme': Drupal.t('Theme', {}, {context: 'Search Filters'}),
+              'news': Drupal.t('News', {}, {context: 'Search Filters'})
+            },
+            filterButtons = $('#search-filters', this).append(filter.createFilterButton('all', filterTitles, 'is-active'));
+
+        // create buttons for available filter types
+        var availableTypes = filter.getAvailableTypes(results);
+        $.each(availableTypes, function(index, type) {
+          filterButtons.append(filter.createFilterButton(type, filterTitles));
+        });
+
+        // reorder buttons
+        filter.reorderButtons(filterButtons, filterTitles);
+
+        // filter results
+        $('a', filterButtons).on('click', function(event) {
+          event.preventDefault();
+          filter.filterResults($(this), results);
+
+          // update result count
+          var resultCountText = Drupal.t('Results (@results)', {'@results': filter.getResultCount(results)}, {context: 'Search Filters'});
+          resultCount.text(resultCountText);
+        });
+      });
+    },
+
+    createFilterButton: function(type, filterTitles, classes) {
+      var buttonClasses = classes ? classes + ' button--small': 'button--small';
+      return '<div class="button-group__button"><a class="' + buttonClasses + '" href="#" data-type="' + type + '">' + filterTitles[type] + '</a></div>';
+    },
+
+    getAvailableTypes: function(results) {
+      var availableTypes = [];
       results.each(function() {
         var type = $(this).attr('data-type');
 
-        var dupe = filterOptions.find(function(item) {
+        var dupe = availableTypes.find(function(item) {
           return item == type;
         });
 
         if (!dupe) {
-          filterOptions.push(type);
-          filterButtons.append(createFilterButton(type));
+          availableTypes.push(type);
         }
       });
+      return availableTypes;
+    },
 
-      // reorder buttons
+    reorderButtons: function(filterButtons, filterTitles) {
       $('div', filterButtons).sort(function(a, b) {
         var aType = $(a).children('a').attr('data-type'),
             bType = $(b).children('a').attr('data-type'),
@@ -48,33 +77,25 @@
         });
         return match;
       }).appendTo(filterButtons);
+    },
 
-      $('a', filterButtons).on('click', function(e) {
-        e.preventDefault();
-        var filterType = $(this).attr('data-type');
-        $(this).addClass('is-active');
-        $(this).parent().siblings().children().removeClass('is-active');
-        results.each(function() {
-          if ($(this).attr('data-type') == filterType || filterType == 'all') {
-            $(this)[0].style.display = 'flex';
-          }
-          else {
-             $(this)[0].style.display = 'none';
-          }
-        });
-        var numResultsText = Drupal.t('Results (@results)', {'@results': getNumResults()}, {context: 'Search Filters'});
-        numResults.text(numResultsText);
+    filterResults: function(button, results) {
+      var filterType = button.attr('data-type');
+      button.addClass('is-active');
+      button.parent().siblings().children().removeClass('is-active');
+
+      results.each(function() {
+        if ($(this).attr('data-type') == filterType || filterType == 'all') {
+          $(this)[0].style.display = 'flex';
+        }
+        else {
+           $(this)[0].style.display = 'none';
+        }
       });
+    },
 
-      function createFilterButton(type, classes) {
-        var buttonClasses = classes ? classes + ' button--small': 'button--small';
-        return '<div class="button-group__button"><a class="' + buttonClasses + '" href="#" data-type="' + type + '">' + filterTitles[type] + '</a></div>';
-      }
-
-      function getNumResults() {
-        return results.filter(':visible').length;
-      }
-
+    getResultCount: function(results) {
+      return results.filter(':visible').length;
     }
   };
 }(jQuery));
