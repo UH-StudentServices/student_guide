8a1d98a36f13d601dd7284d9be8199429fe326bd
Jani Palsamäki <jani.palsamaki@gmail.com>
2017-04-13 15:47:20 +0300

    ActiveLanguage: Added a unit test: shouldNotGetCached

---
+++ modules/uhsg_top_content/tests/src/Unit/Plugin/views/argument_default/ActiveLanguageTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\uhsg_top_content\Plugin\views\argument_default\ActiveLanguage;
+
+/**
+ * @group uhsg
+ */
+class ActiveLanguageTest extends UnitTestCase {
+
+  /** @var ActiveLanguage */
+  private $activeLanguage;
+
+  public function setUp() {
+    $this->activeLanguage = new ActiveLanguage([], NULL, []);
+  }
+
+  /**
+   * @test
+   */
+  public function shouldNotGetCached() {
+    $this->assertEquals(0, $this->activeLanguage->getCacheMaxAge());
+  }
+}
