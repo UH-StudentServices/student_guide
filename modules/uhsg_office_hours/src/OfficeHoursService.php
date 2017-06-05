03110fcb46e79fafb32bd006bd15a5f7273341d9
Jani Palsamäki <jani.palsamaki@gmail.com>
2017-06-05 15:18:30 +0300

    HUB-219: Skeleton for the office hours service. Render the office hours.

---
+++ modules/uhsg_office_hours/src/OfficeHoursService.php
@@ -0,0 +1,31 @@
+<?php
+ 
+namespace Drupal\uhsg_office_hours;
+
+use Drupal\Core\Cache\CacheBackendInterface;
+use GuzzleHttp\Client;
+
+class OfficeHoursService {
+
+  /** @var Client */
+  protected $client;
+
+  public function __construct(Client $client, CacheBackendInterface $cache) {
+    $this->client = $client;
+  }
+
+  public function getOfficeHours() {
+    // TODO: Call the real endpoint when it is ready.
+    //$this->client->get('http://www.example.com');
+
+    // TODO: Use the real data. These are for testing.
+    $officeHours = [
+      ['name' => 'Teacher 1', 'hours' => 'My office hours'],
+      ['name' => 'Teacher 2', 'hours' => 'My office hours'],
+      ['name' => 'Teacher 3', 'hours' => 'My office hours'],
+      ['name' => 'Teacher 4', 'hours' => 'My office hours'],
+    ];
+
+    return $officeHours;
+  }
+}
