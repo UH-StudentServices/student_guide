b604cc15c8f2b5b356ce74ac89acabc1b20e3fa2
Jani Palsamäki <jani.palsamaki@gmail.com>
2017-06-06 11:45:33 +0300

    HUB-219: Refactoring.

--- modules/uhsg_office_hours/src/OfficeHoursService.php
+++ modules/uhsg_office_hours/src/OfficeHoursService.php
@@ -6,6 +6,7 @@ use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\taxonomy\TermInterface;
 use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
 
 class OfficeHoursService {
 
@@ -24,47 +25,59 @@ class OfficeHoursService {
     $this->entityTypeManager = $entityTypeManager;
   }
 
+  /**
+   * @return array
+   */
   public function getOfficeHours() {
+
     // TODO: Call the real endpoint when it is ready.
-    //$apiResponse = $this->client->get('http://www.example.com');
+    $apiResponse = $this->client->get('http://www.example.com');
 
-    // Test:
-    $responseBody = '[
-      {
-        "name": "Olli Opettaja",
-        "officeHours": "Maanantaisin klo 8.00",
-        "degreeProgrammes": ["KH10_001", "KH60_001"]
-      },
-      {
-        "name": "Leila Lehtori",
-        "officeHours": "Parillisten kuukausien kolmas torstai klo 11.15–11.45.",
-        "degreeProgrammes": ["KH60_001"]
-      }
-    ]';
+    return $this->handleResponse($apiResponse);
+  }
 
-    $decodedBody = json_decode($responseBody);
-    $degreeProgrammeCodeTidMap = $this->getDegreeProgrammeCodeTidMap();
+  /**
+   * @param ResponseInterface $apiResponse
+   * @return array
+   */
+  private function handleResponse(ResponseInterface $apiResponse) {
+    if ($apiResponse->getStatusCode() == 200) {
+      $responseBody = $apiResponse->getBody();
 
-    foreach ($decodedBody as $officeHour) {
-      $degreeProgrammeCodes = $officeHour->degreeProgrammes;
-      $degreeProgrammeTids = [];
-
-      foreach ($degreeProgrammeCodes as $degreeProgrammeCode) {
-        if (isset($degreeProgrammeCodeTidMap[$degreeProgrammeCode])) {
-          $degreeProgrammeTids[] = $degreeProgrammeCodeTidMap[$degreeProgrammeCode];
+      // TODO: This is just a test:
+      $responseBody = '[
+        {
+          "name": "Olli Opettaja",
+          "officeHours": "Maanantaisin klo 8.00",
+          "degreeProgrammes": ["KH10_001", "KH60_001"]
+        },
+        {
+          "name": "Leila Lehtori",
+          "officeHours": "Parillisten kuukausien kolmas torstai klo 11.15–11.45.",
+          "degreeProgrammes": ["KH60_001"]
         }
-      }
+      ]';
 
-      $officeHours[] = [
-        'name' => $officeHour->name,
-        'hours' => $officeHour->officeHours,
-        'degree_programme_tids' => implode(',', $degreeProgrammeTids),
-      ];
+      $decodedBody = json_decode($responseBody);
+      $degreeProgrammeCodeTidMap = $this->getDegreeProgrammeCodeTidMap();
+
+      foreach ($decodedBody as $officeHour) {
+        $degreeProgrammeTids = $this->mapDegreeProgrammeCodesToTids($officeHour->degreeProgrammes, $degreeProgrammeCodeTidMap);
+
+        $officeHours[] = [
+          'name' => $officeHour->name,
+          'hours' => $officeHour->officeHours,
+          'degree_programme_tids' => implode(',', $degreeProgrammeTids),
+        ];
+      }
     }
 
     return isset($officeHours) ? $officeHours : [];
   }
 
+  /**
+   * @return array
+   */
   private function getDegreeProgrammeCodeTidMap() {
     $degreeProgrammeTerms = $this->loadAllDegreeProgrammeTerms();
     $degreeProgrammeCodeTidMap = [];
@@ -83,5 +96,22 @@ class OfficeHoursService {
    */
   private function loadAllDegreeProgrammeTerms() {
     return $this->entityTypeManager->getStorage('taxonomy_term')->loadTree('degree_programme', 0, NULL, TRUE);
+  }
+
+  /**
+   * @param array $degreeProgrammeCodes
+   * @param array $degreeProgrammeCodeTidMap
+   * @return array
+   */
+  private function mapDegreeProgrammeCodesToTids(array $degreeProgrammeCodes, array $degreeProgrammeCodeTidMap) {
+    $degreeProgrammeTids = [];
+
+    foreach ($degreeProgrammeCodes as $degreeProgrammeCode) {
+      if (isset($degreeProgrammeCodeTidMap[$degreeProgrammeCode])) {
+        $degreeProgrammeTids[] = $degreeProgrammeCodeTidMap[$degreeProgrammeCode];
+      }
+    }
+
+    return $degreeProgrammeTids;
   }
 }
