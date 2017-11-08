31787fbbb0738e8bea2ee76cee99e285940c1119
Mikael Kundert <mikael.kundert@wunder.io>
2017-11-08 12:22:15 +0200

    HUB-246: Allow acs request to be performed as logged in user also (it is part of login process)

--- modules/uhsg_redirect_to_login/src/Routing/RouteSubscriber.php
+++ modules/uhsg_redirect_to_login/src/Routing/RouteSubscriber.php
@@ -10,13 +10,16 @@ class RouteSubscriber extends RouteSubscriberBase {
    * {@inheritdoc}
    */
   public function alterRoutes(RouteCollection $collection) {
-    if ($route = $collection->get('samlauth.saml_controller_login')) {
-      // HUB-246: In some cases, user may hit the redirect logic during
-      // bootstrapping even when user is logged in. As it is expensive process
-      // to check there whether to redirect or not, we allow user to access saml
-      // login at any role.
-      $anonymousOrAuthenticatedRole = 'anonymous+authenticated';
-      $route->setRequirement('_role', $anonymousOrAuthenticatedRole);
+    $routesToChange = ['samlauth.saml_controller_login', 'samlauth.saml_controller_acs'];
+    foreach ($routesToChange as $routeName) {
+      if ($route = $collection->get($routeName)) {
+        // HUB-246: In some cases, user may hit the redirect logic during
+        // bootstrapping even when user is logged in. As it is expensive process
+        // to check there whether to redirect or not, we allow user to access
+        // saml login at any role.
+        $anonymousOrAuthenticatedRole = 'anonymous+authenticated';
+        $route->setRequirement('_role', $anonymousOrAuthenticatedRole);
+      }
     }
   }
 }
