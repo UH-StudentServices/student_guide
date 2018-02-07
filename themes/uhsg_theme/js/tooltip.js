785997acf922d61599a0197ebdab10472824557e
Jani Palsamäki <jani.palsamaki@gmail.com>
2018-02-07 10:40:34 +0200

    HUB-266: Instructions: Theming (more top padding to box stories to give room for the label). Closing the tooltip on touchstart. Renaming components to use "tooltip" where appropriate. Prevent default event on tooltip label clicks.

--- themes/uhsg_theme/js/tooltip.js
+++ themes/uhsg_theme/js/tooltip.js
@@ -2,7 +2,21 @@
   'use strict';
   Drupal.behaviors.tooltip = {
     attach: function (context, settings) {
-      $(document).tooltip({tooltipClass: "description"});
+      var tooltip = $('.tooltip');
+
+      tooltip.on('click', function (event) {
+        event.preventDefault();
+      });
+
+      tooltip.tooltip({
+        open: function (e, ui) {
+          tooltip.not(this).tooltip('close');
+        }
+      });
+
+      $(document).bind('touchstart click', function (event) {
+        tooltip.tooltip('close');
+      });
     }
   };
 }(jQuery));
