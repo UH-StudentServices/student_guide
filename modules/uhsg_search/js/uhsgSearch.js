a4b4c98fff31517f18e4ac091789b47cd4838aaa
Jani Palsamäki <jani.palsamaki@gmail.com>
2018-04-24 09:52:31 +0300

    HUB-262: Cleaned up old autocomplete related functionality (autocomplete was disabled due to usability issues). Requiring search text (empty searches are no longer possible, since the result set rendering is really slow).

--- modules/uhsg_search/js/uhsgSearch.js
+++ modules/uhsg_search/js/uhsgSearch.js
@@ -1,15 +1,17 @@
 (function ($) {
   'use strict';
-  // Submit search when clicking suggestions.
-  var oldSelect = Drupal.autocomplete.options.select;
-  Drupal.autocomplete.options.select = function () {
-    oldSelect.call(this);
-    if ($(this).hasClass('ui-autocomplete-input')) {
-      $(this).siblings('.form-actions').children('#edit-submit-search').trigger('click');
+  Drupal.behaviors.requireSearchText = {
+    attach: function (context, settings) {
+      var searchField = $('#edit-search-api-fulltext');
+      var searchButton = $('#edit-submit-search');
+
+      if (!searchField.val()) {
+        searchButton.prop('disabled', true);
+      }
+
+      searchField.keyup(function() {
+        searchButton.prop('disabled', !$(this).val());
+      });
     }
-  };
-  // Autocomplete requires the user to click twice in iOS, fix that.
-  Drupal.autocomplete.options.open = function () {
-    $('.ui-autocomplete').off('menufocus hover mouseover');
-  };
+  }
 }(jQuery));
