9b96fe67a7c420f5061d1247c9d8442bb16ec2f9
Jani Palsamäki <jani.palsamaki@gmail.com>
2018-05-31 14:30:53 +0300

    Tests: Added a unit test: OprekServiceTest::getStudyRightsShouldCallAPIUsingClientCertificate

--- modules/uhsg_oprek/tests/src/Unit/Oprek/OprekServiceTest.php
+++ modules/uhsg_oprek/tests/src/Unit/Oprek/OprekServiceTest.php
@@ -1,17 +1,29 @@
 <?php
 
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\ImmutableConfig;
 use Drupal\Tests\UnitTestCase;
 use Drupal\uhsg_oprek\Oprek\OprekService;
 use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 
 /**
  * @group uhsg
  */
 class OprekServiceTest extends UnitTestCase {
 
+  const API_RESPONSE = '{"status": 200}';
+  const BASE_URL = 'baseurl';
+  const CERT_FILEPATH = 'certfilepath';
+  const CERT_KEY_FILEPATH = 'certkeyfilepath';
+  const STUDENT_NUMBER = '123';
+
   /** @var Client */
   private $client;
+
+  /** @var ImmutableConfig */
+  private $config;
 
   /** @var ConfigFactoryInterface */
   private $configFactory;
@@ -19,11 +31,36 @@ class OprekServiceTest extends UnitTestCase {
   /** @var OprekService */
   private $oprekService;
 
+  /** @var ResponseInterface */
+  private $response;
+
+  /** @var StreamInterface */
+  private $stream;
+
   public function setUp() {
     parent::setUp();
 
+    $this->stream = $this->prophesize(StreamInterface::class);
+    $this->stream->getContents()->willReturn(self::API_RESPONSE);
+
+    $this->response = $this->prophesize(ResponseInterface::class);
+    $this->response->getStatusCode()->willReturn(200);
+    $this->response->getBody()->willReturn($this->stream);
+
     $this->client = $this->prophesize(Client::class);
+
+    $this->client->get(
+      self::BASE_URL . '/students/' . self::STUDENT_NUMBER . '/studyrights',
+      ['cert' => self::CERT_FILEPATH, 'ssl_key' => self::CERT_KEY_FILEPATH]
+    )->willReturn($this->response);
+
+    $this->config = $this->prophesize(ImmutableConfig::class);
+    $this->config->get('base_url')->willReturn(self::BASE_URL);
+    $this->config->get('cert_filepath')->willReturn(self::CERT_FILEPATH);
+    $this->config->get('cert_key_filepath')->willReturn(self::CERT_KEY_FILEPATH);
+
     $this->configFactory = $this->prophesize(ConfigFactoryInterface::class);
+    $this->configFactory->get('uhsg_oprek.settings')->willReturn($this->config);
 
     $this->oprekService = new OprekService($this->configFactory->reveal(), $this->client->reveal());
   }
@@ -35,5 +72,17 @@ class OprekServiceTest extends UnitTestCase {
     $this->setExpectedException(\InvalidArgumentException::class);
 
     $this->oprekService->getStudyRights(NULL);
+  }
+
+  /**
+   * @test
+   */
+  public function getStudyRightsShouldCallAPIUsingClientCertificate() {
+    $this->client->get(
+      self::BASE_URL . '/students/' . self::STUDENT_NUMBER . '/studyrights',
+      ['cert' => self::CERT_FILEPATH, 'ssl_key' => self::CERT_KEY_FILEPATH]
+    )->shouldBeCalled();
+
+    $this->oprekService->getStudyRights(self::STUDENT_NUMBER);
   }
 }
