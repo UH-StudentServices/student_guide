bb6a98a8c11dfaec66ae2474e13681d2d10e6940
Jani Palsamäki <jani.palsamaki@gmail.com>
2018-06-01 09:46:18 +0300

    Tests: Added a unit test NewsPerDegreeProgrammeTest::buildShouldReturnEmptyRenderableWhenThereIsNoActiveDegreeProgramme

--- modules/uhsg_news/tests/src/Unit/Plugin/Block/NewsPerDegreeProgrammeTest.php
+++ modules/uhsg_news/tests/src/Unit/Plugin/Block/NewsPerDegreeProgrammeTest.php
@@ -2,6 +2,7 @@
 
 use Drupal\Core\Cache\Context\CacheContextsManager;
 use Drupal\Tests\UnitTestCase;
+use Drupal\uhsg_active_degree_programme\ActiveDegreeProgrammeService;
 use Drupal\uhsg_news\Plugin\Block\NewsPerDegreeProgramme;
 use Prophecy\Argument;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -10,6 +11,9 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * @group uhsg
  */
 class NewsPerDegreeProgrammeTest extends UnitTestCase {
+
+  /** @var ActiveDegreeProgrammeService */
+  private $activeDegreeProgrammeService;
 
   /** @var CacheContextsManager */
   private $cacheContextsManager;
@@ -23,11 +27,14 @@ class NewsPerDegreeProgrammeTest extends UnitTestCase {
   public function setUp() {
     parent::setUp();
 
+    $this->activeDegreeProgrammeService = $this->prophesize(ActiveDegreeProgrammeService::class);
+
     $this->cacheContextsManager = $this->prophesize(CacheContextsManager::class);
     $this->cacheContextsManager->assertValidTokens(Argument::any())->willReturn(TRUE);
 
     $this->container = $this->prophesize(ContainerInterface::class);
     $this->container->get('cache_contexts_manager')->willReturn($this->cacheContextsManager->reveal());
+    $this->container->get('uhsg_active_degree_programme.active_degree_programme')->willReturn($this->activeDegreeProgrammeService->reveal());
 
     Drupal::setContainer($this->container->reveal());
 
@@ -39,6 +46,15 @@ class NewsPerDegreeProgrammeTest extends UnitTestCase {
    */
   public function shouldUseActiveDegreeProgrammeAsCacheContext() {
     $this->assertContains('active_degree_programme', $this->newsPerDegreeProgramme->getCacheContexts());
+  }
+
+  /**
+   * @test
+   */
+  public function buildShouldReturnEmptyRenderableWhenThereIsNoActiveDegreeProgramme() {
+    $this->activeDegreeProgrammeService->getId()->willReturn(NULL);
+
+    $this->assertEquals([], $this->newsPerDegreeProgramme->build());
   }
 }
 
