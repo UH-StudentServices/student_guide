b91c451bd472ccf295836f36bca7ce3bcabd5d6f
Jani Palsamäki <jani.palsamaki@gmail.com>
2018-10-26 09:47:50 +0300

    HUB-347: UserSyncSubscriberFactory: Added MessengerInterface.

--- modules/uhsg_user_sync/src/SamlAuth/UserSyncSubscriberFactory.php
+++ modules/uhsg_user_sync/src/SamlAuth/UserSyncSubscriberFactory.php
@@ -5,6 +5,7 @@ namespace Drupal\uhsg_user_sync\SamlAuth;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Logger\LoggerChannel;
+use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\flag\FlagServiceInterface;
 use Drupal\uhsg_oprek\Oprek\OprekServiceInterface;
 
@@ -13,8 +14,15 @@ use Drupal\uhsg_oprek\Oprek\OprekServiceInterface;
  */
 class UserSyncSubscriberFactory {
 
-  public static function create(ConfigFactoryInterface $configFactory, OprekServiceInterface $oprekService, FlagServiceInterface $flagService, EntityTypeManagerInterface $entityTypeManager, LoggerChannel $logger) {
-    return new UserSyncSubscriber($configFactory, $oprekService, $flagService, $entityTypeManager, $logger);
+  public static function create(
+    ConfigFactoryInterface $configFactory,
+    OprekServiceInterface $oprekService,
+    FlagServiceInterface $flagService,
+    EntityTypeManagerInterface $entityTypeManager,
+    LoggerChannel $logger,
+    MessengerInterface $messenger
+  ) {
+    return new UserSyncSubscriber($configFactory, $oprekService, $flagService, $entityTypeManager, $logger, $messenger);
   }
 
 }
