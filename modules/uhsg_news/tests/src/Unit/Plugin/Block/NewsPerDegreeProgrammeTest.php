398362a3abc9e2e007719f40729a51d98a531e72
Jani Palsamäki <jani.palsamaki@gmail.com>
2018-12-19 13:23:56 +0200

    HUB-392: Do not display news on teacher front page.

--- modules/uhsg_news/tests/src/Unit/Plugin/Block/NewsPerDegreeProgrammeTest.php
+++ modules/uhsg_news/tests/src/Unit/Plugin/Block/NewsPerDegreeProgrammeTest.php
@@ -1,8 +1,13 @@
 <?php
 
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Access\AccessResultAllowed;
+use Drupal\Core\Access\AccessResultForbidden;
 use Drupal\Core\Cache\Context\CacheContextsManager;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\Tests\UnitTestCase;
 use Drupal\uhsg_active_degree_programme\ActiveDegreeProgrammeService;
+use Drupal\uhsg_domain\DomainService;
 use Drupal\uhsg_news\Plugin\Block\NewsPerDegreeProgramme;
 use Prophecy\Argument;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -11,6 +16,9 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * @group uhsg
  */
 class NewsPerDegreeProgrammeTest extends UnitTestCase {
+
+  /** @var AccountInterface*/
+  private $account;
 
   /** @var \Drupal\uhsg_active_degree_programme\ActiveDegreeProgrammeService*/
   private $activeDegreeProgrammeService;
@@ -21,20 +29,27 @@ class NewsPerDegreeProgrammeTest extends UnitTestCase {
   /** @var \Symfony\Component\DependencyInjection\ContainerInterface*/
   private $container;
 
+  /** @var DomainService*/
+  private $domainService;
+
   /** @var \Drupal\uhsg_news\Plugin\Block\NewsPerDegreeProgramme*/
   private $newsPerDegreeProgramme;
 
   public function setUp() {
     parent::setUp();
 
+    $this->account = $this->prophesize(AccountInterface::class);
     $this->activeDegreeProgrammeService = $this->prophesize(ActiveDegreeProgrammeService::class);
 
     $this->cacheContextsManager = $this->prophesize(CacheContextsManager::class);
     $this->cacheContextsManager->assertValidTokens(Argument::any())->willReturn(TRUE);
 
+    $this->domainService = $this->prophesize(DomainService::class);
+
     $this->container = $this->prophesize(ContainerInterface::class);
     $this->container->get('cache_contexts_manager')->willReturn($this->cacheContextsManager->reveal());
     $this->container->get('uhsg_active_degree_programme.active_degree_programme')->willReturn($this->activeDegreeProgrammeService->reveal());
+    $this->container->get('uhsg_domain.domain')->willReturn($this->domainService->reveal());
 
     Drupal::setContainer($this->container->reveal());
 
@@ -57,6 +72,23 @@ class NewsPerDegreeProgrammeTest extends UnitTestCase {
     $this->assertEquals([], $this->newsPerDegreeProgramme->build());
   }
 
+  /**
+   * @test
+   */
+  public function shouldAllowAccessOnStudentDomain() {
+    $this->domainService->isStudentDomain()->willReturn(TRUE);
+
+    $this->assertInstanceOf(AccessResultAllowed::class, $this->newsPerDegreeProgramme->access($this->account->reveal()));
+  }
+
+  /**
+   * @test
+   */
+  public function shouldForbidAccessWhenNotStudentDomain() {
+    $this->domainService->isStudentDomain()->willReturn(FALSE);
+
+    $this->assertInstanceOf(AccessResultForbidden::class, $this->newsPerDegreeProgramme->access($this->account->reveal()));
+  }
 }
 
 /**
