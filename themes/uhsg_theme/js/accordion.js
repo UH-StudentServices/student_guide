119a857c61d0d793a68dd7e2b359a4c63a22f37e
Jani Palsamäki <jani.palsamaki@gmail.com>
2019-03-26 07:29:34 +0200

    HUB-442: Add TAB key code comment

--- themes/uhsg_theme/js/accordion.js
+++ themes/uhsg_theme/js/accordion.js
@@ -6,6 +6,7 @@
         $(this).toggleClass('is-active').next('.accordion__content').toggleClass('visually-hidden');
       });
       $('.accordion__item', context).once().on('keypress', function (event) {
+        // If key is not TAB (fix for Firefox 60.x.xesr).
         if (event.keyCode != 9) {
           event.preventDefault();
           $(this).children('.accordion__title').first().toggleClass('is-active').next('.accordion__content').toggleClass('visually-hidden');
