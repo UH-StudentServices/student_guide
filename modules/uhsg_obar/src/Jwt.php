d58e01a95cb0603343e70ab94fcbd2a0d1f8dc20
Mikael Kundert <mikael.kundert@wunder.io>
2019-06-12 10:11:04 +0300

    HUB-409: Generate required payload paramters
    Require paramters for the payload are:
      - loginEndpoint
      - logoutEndpoint
      - currentLang

--- modules/uhsg_obar/src/Jwt.php
+++ modules/uhsg_obar/src/Jwt.php
@@ -5,6 +5,8 @@ namespace Drupal\uhsg_obar;
 use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Session\AccountInterface;
 use Firebase\JWT\JWT as Firebase_JWT;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Routing\UrlGeneratorInterface;
 
 class Jwt {
 
@@ -14,13 +16,27 @@ class Jwt {
   protected $config;
 
   /**
+   * @var \Drupal\Core\Routing\UrlGeneratorInterface
+   */
+  protected $urlGenerator;
+
+  /**
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
    * Jwt constructor.
    * @param \Drupal\Core\Config\ConfigFactory $configFactory
    * @param \Drupal\Core\Session\AccountInterface $user
+   * @param \Drupal\Core\Routing\UrlGeneratorInterface $urlGenerator
+   * @param \Drupal\Core\Language\LanguageManagerInterface $languageManager
    */
-  public function __construct(ConfigFactory $config, AccountInterface $user) {
+  public function __construct(ConfigFactory $config, AccountInterface $user, UrlGeneratorInterface $urlGenerator, LanguageManagerInterface $languageManager) {
     $this->config = $config->get('uhsg_obar.settings');
     $this->user = $user;
+    $this->urlGenerator = $urlGenerator;
+    $this->languageManager = $languageManager;
   }
 
   public function generateToken() {
@@ -37,10 +53,10 @@ class Jwt {
 
   private function getPayload() {
     return (object) [
-      'loginEndpoint' => '',
-      'logoutEndpoint' => '',
+      'loginEndpoint' => $this->urlGenerator->generateFromRoute('samlauth.saml_controller_login'),
+      'logoutEndpoint' => $this->urlGenerator->generateFromRoute('samlauth.saml_controller_logout'),
       'user' => NULL,
-      'currentLang' => '',
+      'currentLang' => $this->languageManager->getCurrentLanguage()->getId(),
       'languageSelectEndpoints' => '',
     ];
   }
