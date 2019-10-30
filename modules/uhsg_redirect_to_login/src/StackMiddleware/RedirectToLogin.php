3689333ea32430551bbe1e7253e707cb2772a622
Jani Palsamäki <jani.palsamaki@gmail.com>
2019-10-30 10:22:55 +0200

    HUB-459: Temporarily disable login redirect

--- modules/uhsg_redirect_to_login/src/StackMiddleware/RedirectToLogin.php
+++ modules/uhsg_redirect_to_login/src/StackMiddleware/RedirectToLogin.php
@@ -28,7 +28,8 @@ class RedirectToLogin implements HttpKernelInterface {
    */
   public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = TRUE) {
     if ($type == self::MASTER_REQUEST && $this->hasLoggedIn($request) && !$this->hasTriggered($request)) {
-      $response = $this->redirectToLoginResponse($request);
+      // Disable temporarily.
+      // $response = $this->redirectToLoginResponse($request);
     }
     else {
       $response = $this->httpKernel->handle($request, $type, $catch);
