83e574bf85bc5e0a8c200caad5fe4da712549100
Jani Palsamäki <jani.palsamaki@gmail.com>
2019-10-30 12:59:06 +0200

    HUB-459: Configure onelogin/php-saml to use proxy

--- modules/uhsg_samlauth/src/UhsgSamlauthServiceProvider.php
+++ modules/uhsg_samlauth/src/UhsgSamlauthServiceProvider.php
@@ -4,6 +4,7 @@ namespace Drupal\uhsg_samlauth;
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
+use OneLogin\Saml2\Utils;
 use Symfony\Component\DependencyInjection\Reference;
 
 /**
@@ -15,6 +16,10 @@ class UhsgSamlauthServiceProvider extends ServiceProviderBase {
    * {@inheritdoc}
    */
   public function alter(ContainerBuilder $container) {
+
+    // Configure onelogin/php-saml to use proxy.
+    Utils::setProxyVars(TRUE);
+
     // Disable overrides temporarily.
     // $definition = $container->getDefinition('samlauth.saml');
     // $definition->setClass('Drupal\uhsg_samlauth\SamlService');
