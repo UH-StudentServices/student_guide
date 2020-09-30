253eca03f77088a388eb5178447ebfa15ae0b613
Santeri Lindgren <santeri.lindgren@wunder.io>
2020-09-30 16:38:03 +0300

    All code should work now, next we need to make sure it's cleaned up and tested with better specs and users.

--- modules/uhsg_sisu/src/Controller/StudentRightsController.php
+++ modules/uhsg_sisu/src/Controller/StudentRightsController.php
@@ -117,7 +117,7 @@ class StudentRightsController {
   }
 
   /**
-   * Get Primary Study Right.
+   * Get Student Primary Degree Program.
    *
    * @param int $oodiId
    *   User Oodi ID.
@@ -125,11 +125,39 @@ class StudentRightsController {
    * @return \Psr\Http\Message\ResponseInterface
    *   Response object.
    */
-  public function getStudentDegreeProgram($oodiId) {
+  public function getPrimaryStudentDegreeProgram($oodiId) {
     // Need to duplicate this code in php:
     // https://version.helsinki.fi/OPADev/studies/-/blob/master/backend/src/services/users.js#L56
     try {
-      return $this->getStudyRights($oodiId);
+      // Fetch StudyRights from Sisu.
+      $data = $this->getStudyRights($oodiId);
+
+      if(!$data || $data['private_person']) {
+        return null;
+      }
+
+      $studyrightprimalitychain = $data['data']['private_person']['studyRightPrimalityChain'];
+      $studyrights = $data['data']['private_person']['studyRights'];
+
+      // Get primarystudyright from data.
+      $primarystudyright = getPrimaryStudyRight($studyrightprimalitychain, $studyrights);
+
+      // We have no primarystudyrights
+      if(!$primarystudyright) {
+        return null;
+      }
+
+      if($primarystudyright['studyRightGraduation'] && $primarystudyright['studyRightGraduation']['phase1GraduationDate'] && $primarystudyright['acceptedSelectionPath']['educationPhase2']) {
+        // We have graduated from phase1, move to phase2
+        $phase1graduated = TRUE;
+      }
+
+      // if we have graduated then degree program is phase2
+      $degreeprogram = $phase1graduated ? $primarystudyright['acceptedSelectionPath']['educationPhase2'] : $primarystudyright['acceptedSelectionPath']['educationPhase1'];
+      $degreeprogramchild = $phase1graduated ? $primarystudyright['acceptedSelectionPath']['educationPhase2Child'] : $primarystudyright['acceptedSelectionPath']['educationPhase1Child'];
+
+      // Handle specialisation properly
+      return degreeProgramWithSpecialisation($degreeprogram, $degreeprogramchild);
     }
     catch (GuzzleException $e) {
       // Do nothing.
@@ -137,6 +165,68 @@ class StudentRightsController {
 
     $this->log('StudentRightsController encountered an unknown error when retrieving StudyRights for @oodiId', ['@oodiId' => $oodiId], RfcLogLevel::ERROR);
     return FALSE;
+  }
+
+  /**
+   * Get Primary Study Right
+   */
+  private function getPrimaryStudyRight($studyRightPrimalityChain, $studyRights) {
+    // Make sure we have all the needed data.
+    if (!$studyRightPrimalityChain || !$studyRightPrimalityChain['studyRightPrimalities']
+      || !strlen($studyRightPrimalityChain['studyRightPrimalities']) || !$studyRights || !strlen($studyRights)) {
+      return null;
+    }
+
+    // Make sure all data looks ok and return proper id if we have active studyright
+    $studyrightprimalities = $studyRightPrimalityChain['studyRightPrimalities'];
+
+    // Loop trough all studyrightprimalities and find "last" active primality
+    foreach($studyrightprimalities as $id => $studyrightprimality) {
+      if($studyrightprimality['startDate'] && !$studyrightprimality['endDate'] && $studyrightprimality['documentState'] == 'ACTIVE') {
+        $studyRightId = $studyrightprimality['studyRightId'];
+      }
+    }
+
+    // Loop trough studyrights and return the correct one
+    foreach($studyrights as $id => $studyright) {
+      if($studyright['id'] == $studyRightId) {
+        return $studyright;
+      }
+    }
+  }
+
+  /*
+  https://jira.it.helsinki.fi/browse/OPISKELU-506
+  Students of the Faculty of Educational Sciences get special treatment.
+  1) Their specialisation name is added to the name of their degree programme.
+  2) The Guide News are fetched for them using a combination of the degree programme and specialisation codes.
+  An additional twist: the specialisation codes are different in Sisu and Oodi. Guide news API currently only supports Oodi specialisation codes.
+  Since we get the Sisu specialisation module groupId from the study rights, and we have to fetch news using the Oodi code,
+  we need to have a mapping from Sisu module groupId to Oodi specialisation codes.
+  Note: the Sisu module ids in question are the same in QA and production.
+  */
+  private function degreeProgramWithSpecialisation($degreeProgram, $specialisation) {
+    if ($degreeProgram && $specialisation && $specialisation['groupId']) {
+      // Read file
+      $sisu_oodi_codes = json_decode(file_get_contents("./sisu-oodi-codes.json"));
+
+      // Traverse trough all the oodi-sisu mappings.
+      foreach($sisu_oodi_codes as $group) {
+        // If we encounter a mapping that fits, then mark that
+        if($group['groupId'] == $specialisation['groupId']) {
+          $oodiMapping = $group;
+        }
+      }
+
+      // If we have a match, then we need to modify our degreeprogram before returning it.
+      if ($oodiMapping) {
+        $degreeProgram['code'] = $degreeProgram['code'] . $oodiMapping['oodiSpecialisationCode'];
+        $degreeProgram['name'] = $degreeProgram['name'] . $specialisation['name'];
+      }
+    }
+
+    // Return either modified or unmodified degreeprogram
+    return $degreeProgram;
   }
 
   /**
