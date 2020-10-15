8d32fe9153b7480e0904c6d100bdf67a7dd9f0ed
Toni Sinisalo <toni.sinisalo@wunder.io>
2020-10-15 15:16:29 +0300

    HUB-757: Improving site search form accessibility by adding an aria-label to search button and allowing browsers to handle the required search key field validation natively.

--- modules/uhsg_search/js/uhsgSearch.js
+++ modules/uhsg_search/js/uhsgSearch.js
@@ -1,5 +1,6 @@
 (function ($) {
   'use strict';
+
   Drupal.behaviors.requireSearchText = {
     attach: function (context, settings) {
       var self = this;
@@ -7,25 +8,34 @@
       var searchButton = $('#edit-submit-search');
 
       if (!searchField.val()) {
-        searchButton.prop('disabled', true);
+        searchButton.prop('aria-disabled', true);
       }
 
-      searchField.keyup(function(e) {
+      searchButton.on('click', function(e) {
+        self.handleSubmit(e, searchField, searchButton);
+      });
+
+      searchField.on('keyup', function(e) {
         self.handleKeyUp(e, searchField, searchButton);
       });
     },
 
-    handleKeyUp: function(e, searchField, searchButton) {
+    handleSubmit: function(e, searchField, searchButton) {
       var empty = !searchField.val();
-      searchButton.prop('disabled', empty);
-      if (empty && e.which == 13) {
+      if (empty) {
         var originalPlaceholder = searchField.prop('placeholder');
         searchField.prop('placeholder', Drupal.t('Required field'));
         setTimeout(function () {
           searchField.prop('placeholder', originalPlaceholder);
         }, 1000);
       }
+    },
+
+    handleKeyUp: function(e, searchField, searchButton) {
+      var empty = !searchField.val();
+      searchButton.prop('aria-disabled', empty);
       searchField.val(searchField.val().toLowerCase());
     }
   }
+
 }(jQuery));
