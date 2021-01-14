0c4beeabc778c55a30808ac6533331093e7957d1
Toni Sinisalo <toni.sinisalo@wunder.io>
2021-01-14 13:20:26 +0200

    HUB-825: Removing the application specific footer.

--- modules/uhsg_obar/src/Jwt.php
+++ modules/uhsg_obar/src/Jwt.php
@@ -76,8 +76,7 @@ class Jwt {
       'logoutEndpoint' => $this->urlGenerator->generateFromRoute('samlauth.saml_controller_logout'),
       'user' => $this->getUser(),
       'currentLang' => $this->languageManager->getCurrentLanguage()->getId(),
-      'languageSelectEndpoints' => $this->getLanguageSelectEndpoints(),
-      'footer' => $this->getFooter()
+      'languageSelectEndpoints' => $this->getLanguageSelectEndpoints()
     ];
   }
 
@@ -135,100 +134,4 @@ class Jwt {
     }
   }
 
-  /**
-   * The simplest hard-coded implementation for application-specific footer.
-   * Not configurable for now. Links common for all services will be refactored
-   * away in the future, leaving only the truly application-specific
-   * configuration.
-   */
-  private function getFooter() {
-    $footer = [
-      'serviceName' => [
-        'Opiskelu',
-        'Studier',
-        'Studies'
-      ],
-      'applicationName' => [
-        'Opiskelijan ohjeet',
-        'Instruktioner för studerande',
-        'Instructions for students'
-      ],
-      'items' => [
-        'bulletin' => [
-          'name' => [
-            'Tiedotearkisto',
-            'Meddelanden arkiv',
-            'Bulletin archive'
-          ],
-          'url' => [
-            'https://studies.helsinki.fi/ohjeet/news',
-            'https://studies.helsinki.fi/instruktioner/news',
-            'https://studies.helsinki.fi/instructions/news'
-          ]
-        ],
-        'studentServices' => [
-          'name' => [
-            'Opiskelijaneuvonta',
-            'Studentservicen',
-            'Student Services'
-          ],
-          'url' => [
-            'https://studies.helsinki.fi/ohjeet/artikkeli/opiskelijaneuvonta',
-            'https://studies.helsinki.fi/instruktioner/artikel/studentservicen',
-            'https://studies.helsinki.fi/instructions/article/student-services'
-          ]
-        ],
-        'studentServicesAppointment' => [
-          'name' => [
-            'Opiskelijaneuvonnan ajanvaraus',
-            'Tidsbokning till Studentservicen',
-            'Appointments to Student Services'
-          ],
-          'url' => [
-            'https://secure.vihta.com/public-ng/studenthelsinki/#/home',
-            'https://secure.vihta.com/public-ng/studenthelsinki/#/home',
-            'https://secure.vihta.com/public-ng/studenthelsinki/#/home'
-          ]
-        ],
-        'careerServices' => [
-          'name' => [
-            'Urapalvelut',
-            'Karriärservicen',
-            'Career Services'
-          ],
-          'url' => [
-            'https://studies.helsinki.fi/ohjeet/artikkeli/urapalvelujen-ohjaus-some-ja-yhteystiedot',
-            'https://studies.helsinki.fi/instruktioner/artikel/karriarservicens-vagledning-evenemang-och-kontaktuppgifter',
-            'https://studies.helsinki.fi/instructions/article/career-servicesguidance-social-media-and-contact-details'
-          ]
-        ],
-        'exchangeServices' => [
-          'name' => [
-            'Liikkuvuuspalvelut',
-            'Mobilitetsservicen',
-            'International Exchange Services'
-          ],
-          'url' => [
-            'https://studies.helsinki.fi/ohjeet/artikkeli/ota-yhteytta-liikkuvuuspalveluihin',
-            'https://studies.helsinki.fi/instruktioner/artikel/kontakta-mobilitetsservicen',
-            'https://studies.helsinki.fi/instructions/article/contact-international-exchange-services'
-          ]
-        ],
-        'dataProtectionStatement' => [
-          'name' => [
-            'Tietosuojailmoitus',
-            'Dataskyddsmeddelande',
-            'Data Protection Statement'
-          ],
-          'url' => [
-            'https://studies.helsinki.fi/ohjeet/artikkeli/tietosuojailmoitus',
-            'https://studies.helsinki.fi/instruktioner/artikel/dataskyddsmeddelande',
-            'https://studies.helsinki.fi/instructions/article/data-protection-statement'
-          ]
-        ]
-      ]
-    ];
-
-    return (object) $footer;
-  }
 }
